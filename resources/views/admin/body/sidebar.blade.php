cbadb9eb2688345c9a2b0423f749707a1760ccba
speefi <speefi149@gmail.com>
2022-09-11 15:14:53 +0600

    c11comp commit

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -71,7 +71,10 @@
             <li><a href="{{ route('student.class.view') }}"><i class="ti-more"></i>Student Class</a></li>
 
             <li><a href="{{ route('student.year.view') }}"><i class="ti-more"></i>Student Year</a></li>
-          
+
+            <li><a href="{{ route('student.group.view') }}"><i class="ti-more"></i>Student Group</a></li>
+           
+            <li><a href="{{ route('student.shift.view') }}"><i class="ti-more"></i>Student Shift</a></li>
           
             {{-- <li><a href="{{ route('password.view') }}"><i class="ti-more"></i>Change Password</a></li> --}}
             
